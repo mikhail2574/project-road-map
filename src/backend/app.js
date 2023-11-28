4fb1e01490755870d7765df60bcf1570b332aabc
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-11-28 13:54:09 +0200

    web server init

---
+++ src/backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  logger = require('morgan'),
+  { personnelRouter } = require('./routes/api');
+
+const app = express();
+
+const formatLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatLogger));
+app.use(bodyParser.json());
+
+app.use('/api/personnel', personnelRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
