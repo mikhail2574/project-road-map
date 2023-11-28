22a2d28343addb0c22bc5ce98254638b20e2e493
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-11-28 23:29:20 +0100

    add routes

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router';
 import Cars from './Cars';
+import Layout from './Layout';
 
 export const App = () => {
   return (
-    <div>
+    <>
+      <Routes>
+        <Route path="/" element={<h1>MAIN PAGE</h1>} />
+        <Route path="/papers">
+          <Route
+            index
+            path="/main"
+            element={<Layout children={<h2>main!</h2>} />}
+          />
+          <Route path="/car" element={<Layout children={<h2>car!</h2>} />} />
+        </Route>
+      </Routes>
       <Cars />
-    </div>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { App } from './frontend/components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from 'frontend/styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter basename="/project-road-map">
+    <Provider store={store}>
+      <App />
+      <GlobalStyles />
+    </Provider>
+  </BrowserRouter>
 );
