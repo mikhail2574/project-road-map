7d8ad3f1aad0f53e143901658f51903bcb78ff19
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2024-01-13 13:02:30 +0200

    bugfixes and features.
    Fixed bugs with errors in Directory and Personnel components. Refactored error to side effects in operation Thunks if status code 409 user will receive toast.error that car sign or person with that name already exists.
    Added 2 packages react-toastify and react-loader-spinner for better user exp. Also in each of thunks added toast.

---
+++ src/frontend/components/Loader/Loader.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledLoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
